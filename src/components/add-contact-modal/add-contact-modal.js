c2483552ab787e128509cbb200ae96ffc85f66ca
TLIQ <stfakel11@gmail.com>
2021-04-21 03:23:51 +0300

    Урок 8

---
+++ src/components/add-contact-modal/add-contact-modal.js
@@ -0,0 +1,45 @@
+import { Modal as ModalBase } from "@material-ui/core"
+import classnames from "classnames"
+import React from "react"
+import { useDispatch } from "react-redux"
+import { addConversation } from "../../store"
+import styles from "./add-contact-modal.module.css"
+
+const contacts = Array.from({ length: 50 }, (_, i) => `room${i}`)
+
+export const AddContactModal = ({ isOpen, onClose, conversations }) => {
+  const dispatch = useDispatch()
+
+  const handleContactClick = (contact) => {
+    const conversation = conversations.find((item) => item.title === contact)
+    if (!conversation) {
+      dispatch(addConversation(contact))
+      onClose()
+    }
+  }
+
+  const checkActiveConversation = (contact) => {
+    return conversations.find((conversation) => conversation.title === contact)
+  }
+
+  return (
+    <ModalBase open={isOpen} onClose={onClose}>
+      <div className={styles.wrapper}>
+        <h2 className={styles.title}>Добавить учасников</h2>
+        <ul>
+          {contacts.map((contact) => (
+            <li
+              className={classnames({
+                [styles.active]: checkActiveConversation(contact),
+              })}
+              onClick={() => handleContactClick(contact)}
+              key={contact}
+            >
+              {contact}
+            </li>
+          ))}
+        </ul>
+      </div>
+    </ModalBase>
+  )
+}
